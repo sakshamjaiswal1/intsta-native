2798e7acfb48a5c3031bd0436ae0fdd3b4f0f2e8
sakshamjaiswal1 <sjais973@gmail.com>
2023-03-02 11:05:29 +0530

    react-native-setup

---
+++ instaNative/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {SafeAreaView, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>zzzzz</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
